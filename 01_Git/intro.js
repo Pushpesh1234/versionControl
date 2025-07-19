14e6b7508638f6082e7842c0a89a92779c786c65
Pushpesh <pushpeshrawat2001@gmail.com>
2025-07-19 21:18:42 +0530

    commit new files of commands

---
+++ 01_Git/intro.js
@@ -0,0 +1,15 @@
+
+function add(num1,num2,num3){
+    let num1
+    let num2
+    let num3
+    return num1+num2+num3;
+}
+const result=add(5,6,8)
+if(result>0){
+    console.log("sum is positive")
+}
+else{
+    console.log("sum is negative")
+}
+
